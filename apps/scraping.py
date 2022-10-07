b24c7adb15b1a8ccbf5159e93cb5a8a73415ed74
Charlie Beckler <47010787+cbeckler@users.noreply.github.com>
2022-10-07 16:37:05 -0500

    add hemi data to scraping

--- apps/scraping.py
+++ apps/scraping.py
@@ -19,7 +19,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemi_imgs" : mars_hemis(browser)
     }
 
     # Stop webdriver and return data
@@ -97,6 +98,43 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+
+def mars_hemis(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+
+    # get html and store it in hemi_soup
+    html = browser.html
+    hemi_soup = soup(html, 'html.parser')
+
+    # extract info from the thumbnails
+    thumbnails = hemi_soup.find_all('img', class_='thumb')
+
+    # for each image
+    for thumb in thumbnails:
+        hemispheres = {}
+        # extract the src
+        img_url = f"{url}{thumb.get('src')}"
+        # extract the title
+        img_title = thumb.get('alt')
+        img_title = img_title.replace(' thumbnail','')
+        # create temp dict
+        hemispheres['img_url'] = img_url
+        hemispheres['title'] = img_title
+        # append dicts to list
+        hemisphere_image_urls.append(hemispheres)
+
+
+    # 4. return the list that holds the dictionary of each image url and title.
+    return hemisphere_image_urls
 
 
 if __name__ == "__main__":
